4d1e709eca8507b0844540a0b4e71f5160986b85
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2020-06-05 15:19:19 +0200

    add plex link

--- frontend/src/components/FavLinks.jsx
+++ frontend/src/components/FavLinks.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Chip, Avatar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
-import { LocalPlay } from '@material-ui/icons'
+import Icon from '@mdi/react'
+import { mdiAccount } from '@mdi/js'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -14,17 +15,19 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const onClick = (url) => window.open(url, '_blank', 'location=0');
+// const onClick = (url) => window.open(url, '_blank', 'location=0');
+const onClick = (url) => window.open(url, 'new', 'location=0');
 
 const links = [
-  { name: 'Films', url: 'https://www.bbc.co.uk/iplayer/categories/films/most-recent' },
-  { name: 'Comedy', url: 'https://www.bbc.co.uk/iplayer/categories/comedy/most-recent' },
-  { name: 'Drama', url: 'https://www.bbc.co.uk/iplayer/categories/drama-and-soaps/most-recent' },
-  { name: 'Box Sets', url: 'https://www.bbc.co.uk/iplayer/group/p05pn9jr' },
+  { name: 'Films', url: 'https://www.bbc.co.uk/iplayer/categories/films/most-recent', icon: 'mdi mdi-film' },
+  { name: 'Comedy', url: 'https://www.bbc.co.uk/iplayer/categories/comedy/most-recent', icon: 'mdi mdi-film' },
+  { name: 'Drama', url: 'https://www.bbc.co.uk/iplayer/categories/drama-and-soaps/most-recent', icon: 'mdi mdi-film' },
+  { name: 'Box Sets', url: 'https://www.bbc.co.uk/iplayer/group/p05pn9jr', icon: 'mdi mdi-film' },
+  { name: 'Plex', url: 'https://app.plex.tv/', icon: 'mdi mdi-film' },
 ];
 
 const makeChips = (className) => {
-  return links.map(link => <Chip key={link.name} label={link.name} onClick={() => onClick(link.url)} className={className} color="secondary" avatar={<Avatar><LocalPlay /></Avatar>} />);
+  return links.map(link => <Chip key={link.name} label={link.name} onClick={() => onClick(link.url)} className={className} color="secondary" icon={<Icon path={mdiAccount}/>} />);
 };
 
 const FavLinks = () => {
