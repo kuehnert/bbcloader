5b013393575747eb9128a4f7d1750fc596b77922
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2020-06-07 18:22:46 +0200

    prepare for production deploy

---
+++ backend/shipitfile.js
@@ -0,0 +1,42 @@
+module.exports = (shipit) => {
+  require("shipit-deploy")(shipit);
+
+  shipit.initConfig({
+    default: {
+      dirToCopy: "backend",
+      servers: "deploy@mso.onl",
+      repositoryUrl: "https://github.com/kuehnert/bbcloader.git",
+      keepReleases: 5,
+      deleteOnRollback: false,
+    },
+    production: {
+      deployTo: "/home/deploy/sites/bbcloader-api",
+      branch: "develop",
+    },
+  });
+
+  shipit.on("deployed", () => {
+    shipit.start("build");
+    shipit.start("reload");
+  });
+
+  shipit.blTask("build", async () => {
+    await shipit.copyToRemote(
+      `.env.${shipit.config.branch}`,
+      `${shipit.releasePath}/.env`
+    );
+    await shipit.remote(`cd ${shipit.releasePath} && npm install`);
+    await shipit.remote(`cd ${shipit.releasePath} && npm run build`);
+  });
+
+  // shipit.task("reload", async () => {
+  //   await shipit.remote("sudo /home/pi/bin/restart_linkshortener");
+  // });
+};
+
+/*
+  /lib/systemd/system
+  sudo systemctl daemon-reload
+  sudo systemctl enable linkshortener-staging.service
+  sudo systemctl status linkshortener-staging
+*/
