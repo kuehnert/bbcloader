19b732398b381fac818f2642193f9d329d28a82f
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2020-06-14 14:48:36 +0200

    repair fork error type

--- backend/src/utils/startDownload.ts
+++ backend/src/utils/startDownload.ts
@@ -1,17 +1,27 @@
-import { fork } from 'child_process';
+import { fork, Serializable } from 'child_process';
 import { IVideo } from 'src/downloads/downloadSlice';
 
-export function startDownload(config: any, video: IVideo, callback: ({ video: IVideo, error: Error }) => void) {
+class ErrorObject {
+  error?: any;
+}
+
+interface cbParams {
+  video: IVideo;
+  error: string;
+}
+
+export function startDownload(config: any, video: IVideo, callback: ({ video, error }: cbParams) => void) {
   console.log('startDownload');
   const forked = fork('./src/utils/downloadVideo.js');
   forked.send({ msgTyp: 'startDownload', config, video });
 
   forked.on('message', (message) => {
-    if (message) {
-      console.log(`Error downloading ${video.url}. message: '${message}'`);
+    const { error } = message as ErrorObject;
+    if (error) {
+      console.log(`Error downloading ${video.url}. message: '${error}'`);
     }
 
     forked.kill();
-    callback({ video, error: message });
+    callback({ video, error });
   });
 }
